867cff7ca9972a0a9cb310b2eccdfae6bdf2ebd0
Farshid PirahanSiah, PhD <pirahansiah@gmail.com>
2022-01-19 10:10:22 +0100

    update corner detection python

--- CV_metaverse/3D_multi_camera_calibration/corner_detection/cornerDetection.py
+++ CV_metaverse/3D_multi_camera_calibration/corner_detection/cornerDetection.py
@@ -51,4 +51,18 @@ if len(img_main.shape)==3:
     gray_img=cv2.cvtColor(img_src, cv2.COLOR_BGR2GRAY) 
 else:
     gray_img=img_src.copy()
-
+#2
+image_with_corners=gray_img.copy()
+CHECKERBOARD = (6,9)
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+objpoints = []
+imgpoints = []
+objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
+objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+prev_img_shape = None
+ret, corners = cv2.findChessboardCorners(gray_img, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+if ret == True:
+        objpoints.append(objp)
+        corners2 = cv2.cornerSubPix(gray_img, corners, (11,11),(-1,-1), criteria)
+        imgpoints.append(corners2)
+        image_with_corners = cv2.drawChessboardCorners(gray_img, CHECKERBOARD, corners2, ret)
